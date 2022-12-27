a9eca66cd5a872f1615def259dd25393bdc2a9a6
ismaelmosquera <ismael.mosquera@gmail.com>
2022-12-27 21:04:45 +0100

    java imr-lib sources

---
+++ imr_lib/examples/test_matrix/run.sh
@@ -0,0 +1,7 @@
+#
+# Simple script to run test matrix
+#
+cls
+cd bin
+java -classpath .:../../../lib/imr-lib.jar TestMatrix >& out.txt > out.txt
+
---
+++ imr_lib/src/imr/media/audio/package-info.java
@@ -0,0 +1,10 @@
+/**
+* The audio package provides an audio player supporting the following extensions:
+* <ul>
+* <li>mp3</li>
+* <li>wav</li>
+* </ul>
+* The <code>SoundPlayer</code> in the media package is multipurpose and also supports playing MIDI files,
+* but if you only need an audio player to play single files you can choose this option.
+*/
+package imr.media.audio;
