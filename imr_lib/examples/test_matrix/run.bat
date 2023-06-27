fe9361379575a1eca864e57022ddbbd6c070107d
ismaelmosquera <ismael.mosquera@gmail.com>
2023-06-27 19:49:58 +0200

    remove old files

--- imr_lib/examples/test_matrix/run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cls
-cd bin
-java -cp .;..\..\..\lib\imr-lib.jar TestMatrix > out.txt > out.txt
-cd..
--- imr_lib/examples/test_matrix/run.sh
+++
@@ -1,7 +0,0 @@
-#
-# Simple script to run test matrix
-#
-cls
-cd bin
-java -classpath .:../../../lib/imr-lib.jar TestMatrix >& out.txt > out.txt
-
