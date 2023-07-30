4eb6cc145c15d04ba80c090219eaf9bcce57331a
ismaelmosquera <ismael.mosquera@gmail.com>
2023-07-30 19:45:29 +0200

    remove old files

--- imrlib/src/imr/matrix/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
-* The matrix package provides classes to perform the most common operations applied to matrices and vectors.
-* <p>
-* In addition, also implements functionality to solve nxn linear systems and perform LU decomposition.
-*/
-package imr.matrix;
