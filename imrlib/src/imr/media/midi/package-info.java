3c477cf7f386bf12ff38f4dd8bf6c5c761f39cb1
ismaelmosquera <ismael.mosquera@gmail.com>
2023-10-01 13:27:53 +0200

    old lib

--- imrlib/src/imr/media/midi/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
-* The midi package provides classes to manage MIDI data and wrappers to
-* javax.sound.midi classes.
-* <p>
-* In addition, also implements a MIDI file reader and writer and a MIDI player.
-*/
-package imr.media.midi;
