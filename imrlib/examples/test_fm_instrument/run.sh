795b6cb42ebf5d4ed9b7fc9f87732806b46d9eaa
ismaelmosquera <ismael.mosquera@gmail.com>
2023-10-08 14:49:41 +0200

    old lib

--- imrlib/examples/test_fm_instrument/run.sh
+++
@@ -1,8 +0,0 @@
-#
-# Simple script to run test fm instrument
-#
-# The -ea modifier enables assertions
-#
-cls
-cd bin
-java -classpath .:../../../lib/imr-lib.jar -ea TestFMInstrument
--- imrlib/src/imr/util/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
-* The util package provides a useful class to resize, get chunks, clone and print
-* one, two and three-dimensional arrays of any kind of type included the atomic ones.
-* <p>
-* In addition, also offers a class to read strings and numeric data from the command line.
-*/
-package imr.util;
