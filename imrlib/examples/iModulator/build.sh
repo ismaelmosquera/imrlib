b15f2fa18cd046d788a2102501a982540116e814
ismaelmosquera <ismael.mosquera@gmail.com>
2024-05-10 11:53:37 +0200

    old lib

--- imrlib/examples/iModulator/build.sh
+++
@@ -1,5 +0,0 @@
-#
-# Simple script to build modulator example
-#
-cls
-javac -d bin -classpath .:../../lib/imr-lib.jar iModulator.java 
--- imrlib/examples/test_musical_synthesizer/run.sh
+++
@@ -1,8 +0,0 @@
-#
-# Simple script to run test musical instrument
-#
-# The -ea modifier enables assertions
-#
-cls
-cd bin
-java -classpath .:../../../lib/imr-lib.jar -ea TestMusicalInstrument
