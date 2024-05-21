9f2c4b6f7fe068a214e11c6b097edfcc8ccc33f0
ismaelmosquera <ismael.mosquera@gmail.com>
2024-05-21 17:28:47 +0200

    old

--- imrlib/examples/iPlot/signal_plot/run.sh
+++
@@ -1,7 +0,0 @@
-#
-# Simple script to run iSignalPlot
-#
-cls
-cd bin
-java -classpath .:../../../../lib/imr-lib.jar -ea iSignalPlot 
-cd ..
--- imrlib/examples/test_analyzer/run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cls
-cd bin
-java -cp .;..\..\..\ext\mp3plugin.jar;..\..\..\lib\imr-lib.jar -ea TestAnalyzer 
-cd ..
