52860c6b1554a0015e0a090e29d4af74962f70da
ismaelmosquera <ismael.mosquera@gmail.com>
2024-07-26 15:08:19 +0200

    removed

--- imrlib/examples/iPlot/signal_plot/build.sh
+++
@@ -1,7 +0,0 @@
-#
-# Simple script to build iSignalPlot
-#
-echo deleting old files...
-rm bin/*.class
-echo compiling...
-javac -d bin -classpath .:../../../lib/imr-lib.jar iSignalPlot.java
--- imrlib/examples/test_filter/Band_reject/build.sh
+++
@@ -1,7 +0,0 @@
-#
-# Simple script to build TestBandRejectFilter
-#
-echo deleting old files...
-rm bin/*.class
-echo compiling...
-javac -d bin -classpath .:../../../lib/imr-lib.jar TestBandRejectFilter.java
