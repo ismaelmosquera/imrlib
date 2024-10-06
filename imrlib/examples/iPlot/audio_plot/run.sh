995eff247a3d79e031928f3e5436604a0e520b36
ismaelmosquera <ismael.mosquera@gmail.com>
2024-10-06 18:37:50 +0200

    out of date

--- imrlib/examples/iPlot/audio_plot/run.sh
+++
@@ -1,7 +0,0 @@
-#
-# Simple script to run iAudioPlot
-#
-cls
-cd bin
-java -classpath .:../../../../ext/mp3plugin.jar:../../../../lib/imr-lib.jar iAudioPlot 
-cd ..
--- imrlib/examples/test_matrix/run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cls
-cd bin
-java -cp .;..\..\..\lib\imr-lib.jar TestMatrix > out.txt
