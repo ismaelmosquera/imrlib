6e3428d636a001c8e98bb0354ca6c8f0ca6f955d
ismaelmosquera <ismael.mosquera@gmail.com>
2024-10-06 18:50:59 +0200

    imr-lib v0.6

---
+++ imrlib/examples/iPlot/audio_plot/run.sh
@@ -0,0 +1,7 @@
+#
+# Simple script to run iAudioPlot
+#
+cls
+cd bin
+java -classpath .:../../../../ext/mp3plugin.jar:../../../../lib/imr-lib.jar iAudioPlot 
+cd ..
---
+++ imrlib/examples/iRecorder/build.sh
@@ -0,0 +1,5 @@
+#
+# Simple script to build recorder example
+#
+cls
+javac -d bin -classpath .:../../lib/imr-lib.jar iRecorder.java 
