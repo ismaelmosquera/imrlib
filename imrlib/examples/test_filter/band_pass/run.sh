af9b7f5aa45bd593e2f83049db1b0bf8773e6107
ismaelmosquera <ismael.mosquera@gmail.com>
2025-01-08 17:28:38 +0100

    old

--- imrlib/examples/test_filter/band_pass/run.sh
+++
@@ -1,7 +0,0 @@
-#
-# Simple script to run TestBandPassFilter
-#
-cls
-cd bin
-java -classpath .:../../../../lib/imr-lib.jar -ea TestBandPassFilter 
-cd ..
--- imrlib/examples/test_filter/low_pass/run.sh
+++
@@ -1,7 +0,0 @@
-#
-# Simple script to run TestLowPassFilter
-#
-cls
-cd bin
-java -classpath .:../../../../lib/imr-lib.jar -ea TestLowPassFilter 
-cd ..
