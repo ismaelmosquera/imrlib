b189ae8d6881903e0f57354290171753a6cf9707
ismaelmosquera <ismael.mosquera@gmail.com>
2025-02-04 17:15:18 +0100

    bug fixed

--- imrlib/examples/test_analyzer/build.sh
+++
@@ -1,6 +0,0 @@
-#
-# Simple script to build test analyzer example
-#
-cls
-javac -d bin -classpath .:../../lib/imr-lib.jar TestAnalyzer.java
-
--- imrlib/examples/test_oscillator/run.sh
+++
@@ -1,7 +0,0 @@
-#
-# Simple script to run test oscillator
-#
-cls
-cd bin
-java -ea -classpath .:../../../lib/imr-lib.jar TestOscillator
-
