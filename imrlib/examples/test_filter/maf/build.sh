53414106c3fa346e71a26e2c87d9e17b017c40d5
ismaelmosquera <ismael.mosquera@gmail.com>
2025-11-22 10:59:31 +0100

    removed

--- imrlib/examples/test_filter/maf/build.sh
+++
@@ -1,7 +0,0 @@
-#
-# Simple script to build TestMAF
-#
-echo deleting old files...
-rm bin/*.class
-echo compiling...
-javac -d bin -classpath .:../../../lib/imr-lib.jar TestMAF.java
--- imrlib/examples/test_filter/maf/run.sh
+++
@@ -1,7 +0,0 @@
-#
-# Simple script to run TestMAF
-#
-cls
-cd bin
-java -classpath .:../../../../lib/imr-lib.jar -ea TestMAF 
-cd ..
