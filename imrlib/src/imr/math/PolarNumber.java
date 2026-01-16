29433ab1f0a9d380f6be370d3ec3cce387c510a8
ismaelmosquera <ismael.mosquera@gmail.com>
2026-01-16 19:43:24 +0100

    bug fixedÂ

--- imrlib/src/imr/math/PolarNumber.java
+++ imrlib/src/imr/math/PolarNumber.java
@@ -297,9 +297,7 @@ return div(this, p);
 */
 public static PolarNumber add(PolarNumber p1, PolarNumber p2)
 {
-float mag = p1.getMagnitude() + p2.getMagnitude();
-float phase = p1.getArgument();
-return new PolarNumber(mag, phase);
+return p1.toComplex().add(p2.toComplex()).toPolar();
 }
 
 /**
@@ -314,9 +312,7 @@ return new PolarNumber(mag, phase);
 */
 public static PolarNumber sub(PolarNumber p1, PolarNumber p2)
 {
-float mag = p1.getMagnitude() - p2.getMagnitude();
-float phase = p1.getArgument();
-return new PolarNumber(mag, phase);
+return p1.toComplex().sub(p2.toComplex()).toPolar();
 }
 
 /**
